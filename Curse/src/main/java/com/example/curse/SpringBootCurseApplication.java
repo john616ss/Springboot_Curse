e1132784f207125e8b0e67da34c0b443fb60bf01
Juan <dead616ss@gmail.com>
2021-08-10 15:14:49 -0500

    First Project

---
+++ Curse/src/main/java/com/example/curse/SpringBootCurseApplication.java
@@ -0,0 +1,13 @@
+package com.example.curse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurseApplication.class, args);
+	}
+
+}
